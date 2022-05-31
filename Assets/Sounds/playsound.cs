41534efa2a4d67246eeaeacc2a2983e04ff2e228
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-05-31 11:46:54 -0300

    Initial commit

---
+++ Assets/Sounds/playsound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playsound : MonoBehaviour
+{
+    public AudioClip Golpe;
+    public AudioClip Grito;
+
+    AudioSource fuenteAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuenteAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            fuenteAudio.clip = Golpe;
+            fuenteAudio.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            fuenteAudio.clip = Grito;
+            fuenteAudio.Play();
+        }
+    }
+}
